ad6bbef428cb58d34c6cdc15b39c43071db7d8dc
Piotr <Piotrko64@gmail.com>
2021-11-19 00:47:16 +0100

    a href

--- src/components/Header.js
+++ src/components/Header.js
@@ -14,8 +14,8 @@ function Header(props) {
       </div>
       <div className="Title__buttons mt-5">
           <button className="btn btn-outline-primary" onClick={()=>{props.showNewNote()}}> <div className="textmargin">New Note</div>  <div className="btn__icon"><BsFillPlusSquareFill/></div></button>
-          <button className="btn btn-outline-danger"> <div className="textmargin">Piotrko64</div> <div className="btn__icon"><BsFillArrowRightSquareFill/></div></button>
-          <button className="btn btn-secondary"> <div className="textmargin">Github</div><div className="btn__icon"><FaGithub/></div></button>
+          <a href="http://piotrko67.ct8.pl"><button className="btn btn-outline-danger"> <div className="textmargin">Piotrko64</div> <div className="btn__icon"><BsFillArrowRightSquareFill/></div></button></a>
+          <a href="https://github.com/Piotrko64"><button className="btn btn-secondary"> <div className="textmargin">Github</div><div className="btn__icon"><FaGithub/></div></button></a>
       </div>
       </div>
     );
